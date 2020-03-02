b9967431ea90f80256e2fbee61f87dacc917c6dc
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-02 12:47:23 -0500

    created Routing for app and set up redux

--- usemytech/src/App.js
+++ usemytech/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path='/' component={Login} />
+      </Switch>
     </div>
   );
 }
