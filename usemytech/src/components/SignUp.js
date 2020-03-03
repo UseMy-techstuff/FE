74dca27cbb35119e55cd76e636fc8df85215f122
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-03 13:38:44 -0500

    added functionality to login and signup

--- usemytech/src/components/SignUp.js
+++ usemytech/src/components/SignUp.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import {connect} from 'react-redux';
+import { registerUser } from "../reducers/actions/techAction";
+
 import { Button, TextField } from "@material-ui/core";
 import { useForm } from "react-hook-form";
 import { object, string } from "yup";
 
-const SignUp = () => {
+const initialState = {
+  username: "",
+  password: ""
+};
+
+const SignUp = (props) => {
+  const [newUser, setNewUser] = useState(initialState);
+
   const schema = object().shape({
     username: string().required("Username is required"),
     password: string().required("Password is required")
   });
+  
   const { register, handleSubmit, errors } = useForm({
     validationSchema: schema
   });
-  const onSubmit = data => {
-    console.log(data);
+
+  const HandleChange = e => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value
+    });
   };
+
+  const onSubmit = () => {
+    console.log(newUser);
+    props.registerUser(newUser)
+  };
+  
   return (
+    <div>
+    <h1>Sign Up</h1>
     <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
         name="username"
@@ -23,6 +46,7 @@ const SignUp = () => {
         helperText={errors.username ? errors.username.message : ""}
         type="email"
         inputRef={register}
+        onChange={HandleChange}
       />
       <TextField
         name="password"
@@ -31,11 +55,20 @@ const SignUp = () => {
         inputRef={register}
         helperText={errors.password ? errors.password.message : ""}
         type="password"
+        onChange={HandleChange}
       />
       <Button color="primary" type="submit" variant="contained">
         Submit
       </Button>
     </form>
+    </div>
   );
 };
-export default SignUp;
+
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, {registerUser})(SignUp);
--- usemytech/src/reducers/actions/techAction.js
+++ usemytech/src/reducers/actions/techAction.js
@@ -5,16 +5,30 @@ export const TOKEN_AQUIRED = "TOKEN_AQUIRED";
 export const GET_USER = "GET_USER";
 export const GET_STUFF = "GET_STUFF";
 export const SET_ERROR = "SET_ERROR";
+export const NEW_USER = 'NEW_USER';
 
 export const loginData = credential => dispatch => {
   dispatch({ type: SET_TOKEN });
-  axiosWithAuth
-    .post("/stuffs", credential)
+  axiosWithAuth()
+    .post("/users/login", credential)
     .then(res => {
       console.log(res);
-      window.localStorage.setItem("token", res.data.payload);
+      window.localStorage.setItem("token", res.data.token);
       dispatch({ type: TOKEN_AQUIRED });
-      this.props.history.push("/tech-list");
+      this.props.history.push("/user-page");
+    })
+    .catch(err => {
+      console.error("You are getting an error of", err);
+    });
+};
+
+export const registerUser = newUser => dispatch => {
+  dispatch({ type: NEW_USER });
+  axiosWithAuth()
+    .post("/users/register", newUser)
+    .then(res => {
+      console.log(res);
+      this.props.history.push("/");
     })
     .catch(err => {
       console.error("You are getting an error of", err);
@@ -23,7 +37,7 @@ export const loginData = credential => dispatch => {
 
 export const getUser = () => dispatch => {
   dispatch({ type: GET_USER });
-  axiosWithAuth
+  axiosWithAuth()
     .get("/users/:id/stuffs")
     .then(res => {
       console.log(res);
@@ -34,4 +48,3 @@ export const getUser = () => dispatch => {
       dispatch({ type: SET_ERROR, payload: "error fetching data from API!" });
     });
 };
-
