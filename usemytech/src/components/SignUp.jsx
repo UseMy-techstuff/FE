ef64812951f3d6d3cfb9be459a22a98e1f34613b
Stephen M. Gary <stgary0@gmail.com>
2020-03-03 12:14:36 -0500

    Forms

--- usemytech/src/components/SignUp.jsx
+++ usemytech/src/components/SignUp.jsx
@@ -1,84 +1,43 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@material-ui/core';
 import { useForm, Controller } from 'react-hook-form';
-import { Link } from 'react-router-dom';
 import { object, string } from 'yup';
 
-const Login = () => {
+const SignUp = () => {
   const schema = object().shape({
-    Fname: string().required('First name is required'),
-    Lname: string().required('Last name is required'),
-    email: string().required('Email is required'),
+    username: string().required('Username is required'),
     password: string().required('Password is required'),
   });
-  const { register, handleSubmit, errors, control, watch } = useForm({ validationSchema: schema });
+  const { register, handleSubmit, errors, control } = useForm({ validationSchema: schema });
   const onSubmit = (data) => {
     console.log(data);
   };
-
-  const password = useRef({});
-  password.current = watch("password", "");
-  
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
-        name="Fname"
-        error={!!errors.Fname}
-        label="First Name"
-        helperText={errors.Fname ? errors.Fname.message : ''}
-        type="text"
-        inputRef={register}
-        />
-    <TextField
-        name="Lname"
-        error={!!errors.Lname}
-        label="Last Name"
-        inputRef={register}
-        helperText={errors.Lname ? errors.Lname.message : ''}
-        type="text"
-    />
-    <TextField
-        name="email"
-        error={!!errors.email}
-        label="Email"
-        helperText={errors.email ? errors.email.message : ''}
+        name="username"
+        error={!!errors.username}
+        label="Username"
+        helperText={errors.username ? errors.username.message : ''}
         type="email"
         inputRef={register}
-    />
-    <TextField
+      />
+      <TextField
         name="password"
-        error={!!errors.password} 
-        label="Password"
-        inputRef={register({
-            required: "You must specify a password",
-            minLength: {
-              value: 8,
-              message: "Password must have at least 8 characters"
-            }
-          })}
-        helperText={errors.password ? errors.password.message : ''}
-        type="password"
-    />
-    <TextField
-        name="ConfirmPassword"
         error={!!errors.password}
-        label="Confirm Password"
-        inputRef={register({
-            validate: value =>
-              value === password.current || "The passwords do not match"
-          })}
+        label="Password"
+        inputRef={register}
         helperText={errors.password ? errors.password.message : ''}
         type="password"
-    />
+      />
       <Button
         color="primary"
         type="submit"
         variant="contained"
       >
-        Sign up
+        Submit
       </Button>
-      <Link  to="/Login">Sign In</Link>
     </form>
   );
 };
-export default Login;
+export default SignUp;
