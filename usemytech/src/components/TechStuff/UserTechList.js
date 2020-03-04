119bf6f444847c8638aa0cb4590841e57a9eb3eb
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-04 16:00:19 -0500

    created  user tech list

---
+++ usemytech/src/components/TechStuff/UserTechList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import UserItems from './UserItems';
+
+const UserTechList = ({userStuff}) => {
+    console.log(userStuff)
+    return (
+        <div>
+            <h2>My Stuff for Rent</h2>
+            {userStuff.map(tech => 
+                <UserItems key={tech.id} tech={tech} />
+            )}
+        </div>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        userStuff: state.userStuff
+    }
+}
+
+export default connect(mapStateToProps, {})(UserTechList);
